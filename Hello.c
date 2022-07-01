19a212f56701bd8fd9e7066d0fce67548e91b789
Abhijeet Sagar <abhijeetspro39@gmail.com>
2022-07-02 00:32:24 +0530

    adding multiplication in Hello.c

--- Hello.c
+++ Hello.c
@@ -4,7 +4,8 @@ int main(){
     int a = 5;
     int b = 6;
     
-    printf("Sum of 2 numbers are = %d", a + b);
+    printf("Sum of 2 numbers are = %d\n", a + b);
+    printf("Multiple of 2 numbers are = %d", a * b);
 
     return 0;
 }
